9a789ed3973fe023ba946537753b65202b5935b9
ahmadakram1 <51857477+ahmadakram1@users.noreply.github.com>
2023-03-17 01:16:26 +0400

    ahmad

--- src/app/admin/all-patient/all-patient.component.ts
+++ src/app/admin/all-patient/all-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { PatientService } from 'src/app/patient.service';
 
@@ -9,22 +10,50 @@ import { PatientService } from 'src/app/patient.service';
 })
 export class AllPatientComponent {
 
-  @ViewChild("CreateForm") Craete:any;
-  @ViewChild("UpdateForm") Update:any;
-  @ViewChild("DeleteForm") Delete:any;
-  @ViewChild("DetailsForm") Details:any;
-  
-  
-  constructor(public patientService:PatientService ,public dialog:MatDialog){
+  @ViewChild("CreateForm") Craete: any;
+  @ViewChild("UpdateForm") Update: any;
+  @ViewChild("DeleteForm") Delete: any;
+  @ViewChild("DetailsForm") Details: any;
+  UpdatePatientForm = new FormGroup(
+    { patientid:new FormControl(''),
+     email : new FormControl ('', Validators.required),
+     username : new FormControl('',Validators.required)
+   })
+
+
+  constructor(public patientService: PatientService, public dialog: MatDialog) {
 
   }
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.patientService.GetAllPatient()
   }
-  async GetById(patientid:number){
+  async GetById(patientid: number) {
     await this.patientService.GetPatientById(patientid)
     this.dialog.open(this.Details)
+  }
+
+  selectedItem = 0
+  OpenDeleteDialog(patientid: number) {
+    this.selectedItem = patientid
+    this.dialog.open(this.Delete)
+  }
+
+  async DeletePatientbyID() {
+    await this.patientService.DeletePatient(this.selectedItem)
+    this.patientService.GetAllPatient()
+  }
+
+  async OpenUpdateDialog(patientid:number)
+  {
+    await this.patientService.GetPatientById(patientid)
+    this.UpdatePatientForm.patchValue(this.patientService.Patient)
+    this.dialog.open(this.Update)
+  }
+  
+  async UpdatePatient()
+  {
+   await this.patientService.UpdatePatient(this.UpdatePatientForm.value)
+    this.patientService.GetAllPatient()
   }
 }
