02087a4a7868cbb8987b31ccfcd8bb492d84fcba
Yaman1999 <awawdehyaman51@gmail.com>
2023-03-19 02:39:07 +0300

    TTTTT

--- src/app/admin/about-us/about-us.component.ts
+++ src/app/admin/about-us/about-us.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-about-us',
@@ -7,4 +10,55 @@ import { Component } from '@angular/core';
 })
 export class AboutUsComponent {
 
+
+  @ViewChild("UpdateForm") Update: any;
+  @ViewChild("DeleteForm") Delete: any;
+  @ViewChild("DetailsForm") Details: any;
+
+
+
+
+
+constructor(public sharedservice :SharedService,public dialog: MatDialog){
+
+}
+
+
+
+UpdateAboutUsForm = new FormGroup({
+
+  id : new FormControl(""),
+  // image : new FormControl("",Validators.required),
+  paragraph1 : new FormControl("",Validators.required),
+  paragraph2 : new FormControl("",Validators.required)
+
+})
+
+
+
+
+
+
+ngOnInit(){
+  this.sharedservice.GetAllAboutUs();
+}
+
+
+//Update AboutUS
+OpenUpdateDialog(id:number){
+ this.dialog.open(this.Update)
+}
+
+UpdateAboutUs(){
+
+}
+
+OpenDeleteDialog(id:number){
+
+}
+
+
+OpenDetailsDialog(id:number){
+
+}
 }
