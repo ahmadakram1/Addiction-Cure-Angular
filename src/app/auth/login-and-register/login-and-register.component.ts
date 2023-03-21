d5aca9fee09977d6978e11b1ff6d6829935f22f6
ahmadakram1 <51857477+ahmadakram1@users.noreply.github.com>
2023-03-21 23:05:40 +0400

    aaa

--- src/app/auth/login-and-register/login-and-register.component.ts
+++ src/app/auth/login-and-register/login-and-register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-login-and-register',
@@ -7,5 +11,46 @@ import { Component } from '@angular/core';
 })
 export class LoginAndRegisterComponent {
 
+  constructor(public shaerdService: SharedService, private toastr: ToastrService, private spinner: NgxSpinnerService) { }
 
+  LoginForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    }
+  );
+
+  RegisterForm = new FormGroup({
+
+    firstname: new FormControl("", [Validators.required]),
+    lastname: new FormControl("", [Validators.required]),
+    username: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+    //file : new FormControl("",[Validators.required])
+
+  })
+
+  Login() {
+    this.shaerdService.Login(this.LoginForm.value)
+  }
+
+  async Register() {
+    console.log(this.RegisterForm.value)
+    let data = this.RegisterForm.value
+    console.log(data);
+    await this.shaerdService.RegisterPatient(data)
+    console.log(this.shaerdService.imageName);
+  }
+
+  UploadImage(input: any) // <input>
+  {
+    if (input.files.length != 0) {
+      let uploadedFile = input.files[0] // imagefile 
+      let formData = new FormData()
+      formData.append('file', uploadedFile)
+      this.shaerdService.UploadImage(formData)
+    }
+  }
+  GetData() { console.log(this.RegisterForm.value); }
 }
