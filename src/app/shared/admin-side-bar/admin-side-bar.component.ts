f48c5054b1d0cf9054f1a6f12a758697e02c4db7
Yaman1999 <awawdehyaman51@gmail.com>
2023-03-23 02:21:44 +0300

    s

--- src/app/shared/admin-side-bar/admin-side-bar.component.ts
+++ src/app/shared/admin-side-bar/admin-side-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AdminService } from 'src/app/admin.service';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-admin-side-bar',
@@ -7,8 +8,16 @@ import { AdminService } from 'src/app/admin.service';
   styleUrls: ['./admin-side-bar.component.css']
 })
 export class AdminSideBarComponent {
-  constructor(public adminService:AdminService ){
+  constructor(public adminService:AdminService,public sharedservice:SharedService ){
 
   }
 
-}
+  
+  async ngOnInit() {
+
+
+    this.sharedservice.getDoctodid(localStorage.getItem("loginid")?.toString())
+   await this.adminService.GetDoctorById(this.sharedservice.doctodid)
+
+
+}}
