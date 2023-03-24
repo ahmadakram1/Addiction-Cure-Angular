14c38a5fbe716c6e4f28a2c7ddd32337b873e73c
ahmadakram1 <51857477+ahmadakram1@users.noreply.github.com>
2023-03-25 01:30:28 +0400

    aa

--- src/app/admin/main/main.component.ts
+++ src/app/admin/main/main.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { AdminService } from 'src/app/admin.service';
 import { PatientService } from 'src/app/patient.service';
 import { SharedService } from 'src/app/shared.service';
-
-// import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-main',
@@ -25,33 +25,45 @@ async ngOnInit(){
   this.adminService.GetAllPayment()
   this.sharedservice.getDoctodid(localStorage.getItem("loginid")?.toString())
   
-}
-// title = 'ng2-charts-demo';
 
-  // public lineChartData: ChartConfiguration<'line'>['data'] = {
-  //   labels: [
-  //     'January',
-  //     'February',
-  //     'March',
-  //     'April',
-  //     'May',
-  //     'June',
-  //     'July'
-  //   ],
-  //   datasets: [
-  //     {
-  //       data: [ 65, 59, 80, 81, 56, 55, 40 ],
-  //       label: 'Series A',
-  //       fill: true,
-  //       tension: 0.5,
-  //       borderColor: 'black',
-  //       backgroundColor: 'rgba(255,0,0,0.3)'
-  //     }
-  //   ]
-  // };
-  // public lineChartOptions: ChartOptions<'line'> = {
-  //   responsive: false
-  // };
-  // public lineChartLegend = true;
+
+  var myChart = new Chart("myChart", {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
 }
+
+
+}
+
