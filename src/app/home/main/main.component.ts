3a8ed840f450d487eb09d1229f4314fea65bbe66
ahmadakram1 <51857477+ahmadakram1@users.noreply.github.com>
2023-03-25 02:54:51 +0400

    aaaa

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -29,60 +29,6 @@ export class MainComponent {
   }
  
 
-  items = [
-    {
-      img: 'assets/HomeAssets/img/team-2.jpg',
-      fname: 'ahmad',
-      lname: 'ahmad',
-      specialty: 'bone',
-      Description:
-        'Dolor lorem eos dolor duo eirmod sea. Dolor sit magna rebum clita rebum dolor Dolor lorem eos dolor duo eirmod sea. Dolor sit magna rebum clita rebum dolor Dolor lorem eos dolor duo eirmod sea. Dolor sit magna rebum clita rebum dolor',
-    },
-    {
-      img: 'assets/HomeAssets/img/team-3.jpg',
-      fname: 'sara',
-      lname: 'ahmad',
-      specialty: 'bone',
-      Description:
-        'Dolor lorem eos dolor duo eirmod sea. Dolor sit magna rebum clita rebum dolor Dolor lorem eos dolor duo eirmod sea. Dolor sit magna rebum clita rebum dolor Dolor lorem eos dolor duo eirmod sea. Dolor sit magna rebum clita rebum dolor',
-    },
-    {
-      img: 'assets/HomeAssets/img/team-1.jpg',
-      fname: 'yaman',
-      lname: 'ahmad',
-      specialty: 'bone',
-      Description:
-        'Dolor lorem eos dolor duo eirmod sea. Dolor sit magna rebum clita rebum dolor Dolor lorem eos dolor duo eirmod sea. Dolor sit magna rebum clita rebum dolor Dolor lorem eos dolor duo eirmod sea. Dolor sit magna rebum clita rebum dolor ',
-    },
-  ];
-
-
-  public data :any;
-  
-  GetCardData(Card : any){
-    console.log('brefor');
-    console.log(this.data);
-    this.data = Card;
-    console.log('after');
-    console.log(this.data);
-    
-    console.log(Card); 
-    const CardData = {
-      img: Card.img,
-      fname: Card.fname,
-      lname: Card.lname,
-      specialty: Card.specialty,
-      Description: Card.Description
-    }
-    this.sendvalue.emit(CardData)
-    localStorage.setItem('img' , this.data.img);
-    localStorage.setItem('fname' , this.data.fname);
-    localStorage.setItem('lname' , this.data.lname);
-    localStorage.setItem('specialty' , this.data.specialty);
-    localStorage.setItem('Description' , this.data.Description);
-
-  }
-
   CreateContactUsForm = new FormGroup({
 
     name: new FormControl("", Validators.required),
