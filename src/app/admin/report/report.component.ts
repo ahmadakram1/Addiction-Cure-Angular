05089492582da3fc6af653f43d039d001d39345c
aboodghx <abedabood009@gmail.com>
2023-03-25 02:55:28 +0300

    5

--- src/app/admin/report/report.component.ts
+++ src/app/admin/report/report.component.ts
@@ -52,27 +52,30 @@ console.log("as"+this.adminService.reports.length);
   var myChart = new Chart("myChart", {
     type: 'bar',
     data: {
-        labels: ['Red'],
+        labels: ['Red','blue'],
         datasets: [{
             label: '# of Votes',
-            data: [this.adminService.reports.length],
+            data: [this.adminService.reports.length,
+              this.adminService.reports.reduce((sum:any,obj:any)=>sum+obj.amount,0)
+              ,this.adminService.reports.reduce((sum:any,obj:any)=>sum+obj.numberoftest,0)
+            ],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                // 'rgba(54, 162, 235, 0.2)',
-                // 'rgba(255, 206, 86, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
                 // 'rgba(75, 192, 192, 0.2)',
                 // 'rgba(153, 102, 255, 0.2)',
                 // 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                // 'rgba(54, 162, 235, 1)',
-                // 'rgba(255, 206, 86, 1)',
+                 'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
                 // 'rgba(75, 192, 192, 1)',
                 // 'rgba(153, 102, 255, 1)',
                 // 'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 2
         }]
     },
     options: {
